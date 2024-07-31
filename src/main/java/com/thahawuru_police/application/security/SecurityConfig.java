c6f4189264a4bf3233efeba3733ce435d87f0af9
Sonal Induwara <sonalinduwara@gmail.com>
2024-07-31 15:39:30 +0530

    origin

--- src/main/java/com/thahawuru_police/application/security/SecurityConfig.java
+++ src/main/java/com/thahawuru_police/application/security/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -33,6 +38,19 @@ public class SecurityConfig {
                 .httpBasic(withDefaults());
         http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:8081","http://localhost:8082","http://localhost:5000","http://localhost:3000","http://localhost:3001","http://localhost:3010"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
